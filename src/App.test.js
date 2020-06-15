0ab8e02cdc5b4ad497e985245a9b3f8f52db05de
Luke Adams <muhammadadams@gmail.com>
2020-06-15 15:06:06 -0400

    mvp

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,24 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  render(<ContactForm />);
+});
+
+test("renders input fields", () => {
+  const { getByText, getByLabelText } = render(<ContactForm />);
+  const firstName = getByLabelText(/First Name/i);
+  const lastName = getByText(/Last Name/i);
+  expect(firstName).toBeInTheDocument();
+  expect(lastName).toBeInTheDocument();
+});
+
+//test("inputs");
+test("testing submit", () => {
+  const { getByLabelText, getByText } = render(<ContactForm />);
+  const firstName = getByLabelText(/First Name/i);
+  const lastName = getByText(/Last Name/i);
+  fireEvent.change(firstName, { name: "firstName", value: "" });
+  fireEvent.change(lastName, { name: "lastName", value: "" });
 });
